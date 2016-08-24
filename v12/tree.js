536ba921923c6c6e25b06cdcf26c27a90c653028
Chet Corcos <ccorcos@gmail.com>
2016-08-24 01:39:12 -0700

    lazy tree

--- v12/tree.js
+++ v12/tree.js
@@ -133,6 +133,18 @@ const zip = R.curry((list1, list2) => {
 //     just continue to evaluate the tree the node
 //   else if the node isnt a thunk
 
+// there seems to be a difference between a diff-reduce and a diff-patch. diff reduce will
+// reduce over the values producing some new result. this is great for publish and hotkeys
+// but so useful when it comes to virtual dom or... something along those lines. i dont actually
+// have a compelling example yet. websocket channels or anythere where you want to dedupe
+// will use diff-reduce. diff-patch is really only useful for vdom I suppose... even graphql
+// is going to involve deduping and then doing a separate diff... in the end, this is a
+// lazy reduce, not a diff!
+
+// we can definitely refactor out a lot of this logic around what is a thunk and how we save
+// all the data in the previous object. this can all be generalized to work for anyone's
+// problem :)
+
 const lazyReduce = (previous) => R.curry((fn, init, node) => {
   if (previous) {
     if (node.__type === 'thunk') {
@@ -193,3 +205,111 @@ const lazyReduce = (previous) => R.curry((fn, init, node) => {
     }
   }
 })
+
+// computation :: {thunk?, node, result, children?}
+// node :: {value, children?}
+// if a node has no children then result is the node value. otherwise its the
+// reduction of applying fn over the all the child node results and the node value.
+const lazyReduce = R.curry((fn, prev, next) => {
+  if (prev) {
+    if (isThunk(next)) {
+      const thunk = next
+      if (compHasThunk(prev) && thunkEquals(thunk, compGetThunk(prev))) {
+        return prev
+      } else {
+        const node = runThunk(thunk)
+        if (node === compGetNode(prev)) {
+          return prev
+        } else if (hasChildren(node)) {
+          const children = zip(
+            compGetChildren(prev) || [],
+            getChildren(node),
+          ).map(([p, c]) =>
+            lazyReduce(fn, p, c)
+          )
+          const result = children.reduce((acc, child) => fn(acc, compGetResult(child)), getValue(node))
+          return computation({
+            thunk: thunk,
+            node: node,
+            result: result,
+            childen: children,
+          })
+        } else {
+          return computation({
+            thunk: thunk,
+            node: node,
+            result: getValue(node),
+          })
+        }
+      }
+    } else {
+      const node = next
+      if (compGetNode(prev) === node) {
+        return prev
+      } else {
+        if (hasChildren(node)) {
+          const children = zip(
+            compGetChildren(prev) || [],
+            getChildren(node),
+          ).map(([p, c]) =>
+            lazyReduce(fn, p, c)
+          )
+          const result = children.reduce((acc, child) => fn(acc, compGetResult(child)), getValue(node))
+          return computation({
+            node: node,
+            result: result,
+            childen: children,
+          })
+        } else {
+          return computation({
+            node: node,
+            result: getValue(node),
+          })
+        }
+      }
+    }
+  } else {
+    if (isThunk(next)) {
+      const thunk = next
+      const node = runThunk(thunk)
+      if (hasChildren(node)) {
+        const children = getChildren(node).map(c => lazyReduce(fn, undefined, c))
+        const result = children.reduce((acc, child) => fn(acc, compGetResult(child)), getValue(node))
+        return computation({
+          thunk,
+          node,
+          result,
+          children
+        })
+      } else {
+        const result = getValue(result)
+        return computation({
+          thunk,
+          node,
+          result,
+        })
+      }
+    } else {
+      const node = next
+      if (hasChildren(node)) {
+        const children = getChildren(node).map(c => lazyReduce(fn, undefined, c))
+        const result = children.reduce((acc, child) => fn(acc, compGetResult(child)), getValue(node))
+        return computation({
+          node,
+          result,
+          children
+        })
+      } else {
+        const result = getValue(result)
+        return computation({
+          node,
+          result,
+        })
+      }
+    }
+  }
+})
+
+// TODO: refactor some more. its pretty ugly and not reusing enough. dont need to be
+// quite so generic either but may as well.
+// make some test that demonstrate that this function works
